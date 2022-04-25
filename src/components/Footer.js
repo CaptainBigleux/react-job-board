fce16151d6952f5b795b8b971eaa72eec138964c
Adrien Callioni <adrien.callioni@gmail.com>
2022-04-25 16:50:20 +0200

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
+      Made with&nbsp;<span>React</span>&nbsp;at&nbsp;<span>Le Reacteur</span>
+      &nbsp;by&nbsp;
       <span>Bibi</span>
     </footer>
   );
